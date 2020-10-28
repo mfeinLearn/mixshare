9e930ccbcc4da3f52650e4b1db90fd1004cfe512
github username <github email address>
2020-10-28 15:47:27 -0400

    fin with sprint 1 - user registration form and actions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,9 @@ class ApplicationController < Sinatra::Base
 register Sinatra::ActiveRecordExtension
 
   configure do
+    enable :sessions
+    set :session_secret, "mixshareapp-1234"
+
     set :public_folder, 'public'
     set :views, 'app/views'
   end
@@ -10,4 +13,31 @@ register Sinatra::ActiveRecordExtension
     erb :welcome
   end
 
+      helpers do
+          def logged_in?
+              !!current_user
+          end
+          # !! - is a way of converting truthy and falsey values to honest to god true or false
+          #=> nil !nil => true => !true => false
+          #=> #=> #<User> !#<User> => false !false => true
+          # true if this method was able to populate this instance variable (@user)
+          # false if what ever reason it was not
+
+          def current_user
+              # if their is a value in session user id try to find the user whos id is equal to that session user_id
+              ## and assign that user to the instance variable @user
+              # -----------------
+              # Because @user was not populated ||= (this or equals) would trigger the right side
+              # which would ask is their a session user id if so find that user and assign it to the instance
+              # variable
+              # this will return nil if their is not user id in the session
+              # or it will return what ever user was found
+              @current_user ||= User.find(session[:user_id]) if session[:user_id]
+          end
+
+      end
 end
+
+## All of the methods defined inside of the block of helpers
+# are accessable to the views and to the controllers of your application.
+# - consolidates some common functionality
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,4 +17,28 @@ class UsersController < ApplicationController
         end
     end
 
+    get '/login' do
+        erb :'users/login.html'
+    end
+
+    post '/login' do
+        binding.pry
+        # this does not actually log them in this actually confirms that they entered in the correct info for their login
+        @user = User.find_by(:email => params[:email])
+        if @user && @user.authenticate(params[:password])# provided by has_secure_password encript password and takes care of
+            ## the encription of their password agenst their plan text
+            # assuming they logged in correctly
+            session["user_id"] = @user.id
+            redirect "/"
+        else
+            erb :'users/login.html'
+        end
+    end
+
+    get '/logout' do
+        session.clear # this will erase the users user_id
+        redirect '/'
+    end
 end
+# Do this later!! VV
+# to log them in we have to put something in there session or cokiees to remember that they are logged in
